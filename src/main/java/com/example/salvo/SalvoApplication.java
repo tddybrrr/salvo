ff8d54cc732818c853493cddab7ae36b84b90dc2
teddy <theodorelrose@gmail.com>
2019-03-28 10:56:36 +0100

    Initial Commit: Simple battleship game

---
+++ src/main/java/com/example/salvo/SalvoApplication.java
@@ -0,0 +1,33 @@
+package com.example.salvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
+
+
+
+@SpringBootApplication
+public class SalvoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalvoApplication.class, args);
+	}
+		@Bean
+		public CommandLineRunner initData(PlayerRepository repository) {
+			return (args) -> {
+				// save a couple of customers
+				repository.save(new Player("Jack", "Bauer"));
+				repository.save(new Player("Chloe", "O'Brian"));
+				repository.save(new Player("Kim", "Bauer"));
+				repository.save(new Player("David", "Palmer"));
+				repository.save(new Player("Michelle", "Dessler"));
+			};
+		}
+
+
+	}
+
+
+
