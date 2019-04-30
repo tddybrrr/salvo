41441851cce2cfc5651b41c685922212a9fdde72
teddy <theodorelrose@gmail.com>
2019-04-30 17:36:47 +0200

    updated with functions to run based on whether someone is signed in already

--- src/main/resources/static/web/game.js
+++ src/main/resources/static/web/game.js
@@ -1,11 +1,41 @@
 
+/// run a function to test whether anyone is logged in
+tester();
+
+function tester(){
+    fetch('/api/games', {
+              credentials: 'include',
+              method: 'POST',
+              headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/x-www-form-urlencoded'
+              }
+          })
+          .then(response => response.json())
+          .then(response => {
+          //if some one is logged in, build the user interface
+              if(response.player != null){
+                console.log("someone is looged in")
+                buildUI();
+              //if some no one is logged in, build the sign-up form
+              } else {
+                console.log("no one is logged in");
+                buildLoginPanel();
+              }
+          })
+          .catch(err => console.log(err))
+}
 
 
-let form = document.getElementById('myForm');
-
+function buildLoginPanel(){
+     var wrapper = document.getElementById("wrapper");
+    wrapper.innerHTML = "";
+    wrapper.classList.add('loginStyle');
+    wrapper.innerHTML='<form class="form-group w-50 p-3" id="myForm" name="myForm"> <div class="row"> <div class="col"> Enter Username: <input class="form-control" type="text" id="username" name="userName"> </div> <div class="col"> Enter password: <input class="form-control" type="text" id="password" name="password"> </div> </div> <div class="row"> <button id = "signinButton" type="button" class="btn btn-primary col m-3" onclick="testLogin()">Sign in</button> <button id = "signupButton" type="button" class="btn btn-success col m-3" onclick="testSignup()">Sign up</button> </div> </form>';
+}
 
 function testSignup() {
-
+    let form = document.getElementById('myForm');
   fetch('/api/players', {
           credentials: 'include',
           method: 'POST',
@@ -19,15 +49,14 @@ function testSignup() {
       .then(response => {
           console.log(response);
           if (response != null){
-              setTimeout(testLogin,2000);
+              testLogin();
           }
       })
       .catch(err => console.log(err))
 }
 
-
 function testLogin() {
-
+let form = document.getElementById('myForm');
   fetch('/api/login', {
           credentials: 'include',
           method: 'POST',
@@ -37,16 +66,37 @@ function testLogin() {
           },
           body: 'userName=' + form[0].value + '&password=' + form[1].value
       })
+//      .then(response => response.json())
       .then(response => {
-          console.log(response);
+          console.log(response.status)
           if(response.ok){
-              console.log("success!");
-              document.location.reload(true);
+               location.reload();
           }
       })
       .catch(err => console.log(err))
 }
 
+
+//------------------------------------------------------------
+//Building the user interface for a logged in player
+
+function buildUI(){
+    var wrapper = document.getElementById("wrapper");
+    wrapper.innerHTML = "";
+    wrapper.innerHTML = ' <div id="lowerSection"> <div id="tableWrapper"> <button id = "logoutButton" type="button" class="btn btn-secondary m-3 logoutButton" onclick="testLogout()"> LOG OUT</button> </div> </div>';
+    var somediv = document.createElement('div');
+    var body = document.getElementById("body");
+    body.appendChild(somediv);
+    somediv.innerHTML = '<div class="form-group"> <label for="exampleFormControlSelect1">Example select</label> <select class="form-control" id="gadget" name="gadget"> <option>1</option> <option>2</option> <option>3</option> <option>4</option> <option>5</option> </select> <button id="btn">Show selected games</button></div>';
+    document.getElementById('btn').addEventListener('click', show_selected);
+
+}
+
+function show_selected() {
+    var selector = document.getElementById('gadget');
+    var value = selector[selector.selectedIndex].value;
+    console.log(value);
+}
 
 function testLogout(){
 
@@ -68,134 +118,6 @@ function testLogout(){
           .catch(err => console.log(err))
     }
 
-
- // get url of page to pull correct data from api
-var gameNumIndex = window.location.href.indexOf("g=");
-var gameNum = window.location.href.slice(gameNumIndex+2);
-
-if (window.location.href.includes("g=")){
-
-    fetch( "http://localhost:8080/api/game_view/"+gameNum).then(function(response) {
-      if (response.ok) {
-      // add a new promise to the chain
-        return response.json();
-      }
-      // signal a server error to the chain
-      throw new Error(response.statusText);
-    }).then(function(json) {
-
-        console.log(json);
-        buildTable(json);
-        buildGrid(json.GamePlayersInThisGame[0], "gp1Grid", json.GamePlayersInThisGame[1].salvoes);
-        buildGrid(json.GamePlayersInThisGame[1], "gp2Grid", json.GamePlayersInThisGame[0].salvoes);
-
-    }).catch(function(error) {
-      // called when an error occurs anywhere in the chain
-      console.log( "Request failed: " + error.message );
-    });
-}
-
-function buildGrid(data, location, opponentsShots){
-
-    var gridDiv = document.getElementById(location);
-    gridTitle = document.createElement("h1");
-    gridTitle.innerHTML = "gp" + data.gpID + "'s Grid";
-    gridDiv.appendChild(gridTitle);
-
-    var grid = document.createElement("table");
-    gridDiv.appendChild(grid);
-
-    var letters = ["/", "a", "b", "c", "d", "e", "f", "g", "h"];
-
-    for (i=0; i< 9; i++){
-        var row = grid.insertRow(i);
-        // first row of letters
-        if (i==0){
-            for (var c=0; c<9; c++){
-                var cell = row.insertCell(c);
-                cell.innerHTML=letters[c];
-                cell.style.color="green";
-                cell.style.fontWeight="bold";
-            }
-        // rest of the rows
-        } else if (i>0) {
-            // fill left-most cell with row number
-            var cell = row.insertCell(0);
-            cell.innerHTML=i;
-            cell.style.color="green";
-            cell.style.fontWeight="bold";
-            // fill main grid with letter/number coordinates
-            for (var g=1; g<9; g++){
-                var cell = row.insertCell(g);
-
-                for (p=0; p<data.ships.length; p++){
-                    for (x=0; x<data.ships[p].location.length; x++){
-                         if (data.ships[p].location[x] == letters[g]+i) {
-                            cell.style.backgroundColor = "red";
-                         }
-                         for (b=0; b<opponentsShots.length; b++){
-                            for (h=0; h < opponentsShots[b].location.length; h++){
-                                if (opponentsShots[b].location[h] == letters[g]+i){
-                                cell.innerHTML = "x" + opponentsShots[b].turn;
-
-                                    if (data.ships[p].location[x] == opponentsShots[b].location[h]){
-                                        cell.style.backgroundColor = "orange";
-                                    }
-                                }
-                            }
-                         }
-                    }
-                }
-            }
-        }
-    }
-}
-
-
-function buildTable(data){
-
-    var gameID = document.getElementById("gameID");
-
-    gameID.innerHTML=data.gameID;
-
-    var body = document.getElementById("theData");
-
-    var gameLoopLength = data.GamePlayersInThisGame.length;
-
-    var rowIterator = 0;
-
-    for (i=0; i<2; i++){
-
-    // fill the first two columns with game player data
-         var row = body.insertRow(rowIterator);
-         var cell = row.insertCell(0);
-         cell.innerHTML = data.GamePlayersInThisGame[i].gpID;
-
-         var cell = row.insertCell(1);
-         cell.innerHTML = data.GamePlayersInThisGame[i].playerName;
-
-         // Fill the second two colums with ship data.
-         for (var j=0; j < data.GamePlayersInThisGame[i].ships.length;){
-                 var cell = row.insertCell(2);
-                 cell.innerHTML = data.GamePlayersInThisGame[i].ships[j].type;
-                 var cell = row.insertCell(3);
-                 cell.innerHTML = data.GamePlayersInThisGame[i].ships[j].location;
-                    j++
-
-                    // are there more ships for this gameplayer?
-                 if (j < data.GamePlayersInThisGame[i].ships.length){
-                        rowIterator++;
-                        var row = body.insertRow(rowIterator);
-                        var cell = row.insertCell(0);
-                        cell.innerHTML = "";
-                        var cell = row.insertCell(1);
-                        cell.innerHTML = "";
-                }
-         }
-         rowIterator++;
-    }
- };
-
 //-----------------------------------------------------------------------------------
 
 var gpNumIndex = window.location.href.indexOf("gp=");
@@ -216,8 +138,6 @@ fetch( "http://localhost:8080/api/gp_view/"+gpNum).then(function(response) {
 
         console.log(json);
         firstPersonView(json)
-
-
     }).catch(function(error) {
       // called when an error occurs anywhere in the chain
       console.log( "Request failed: " + error.message );
@@ -230,21 +150,22 @@ function firstPersonView(data){
     daGrids.id="daGrids";
 
     var myGrid = document.createElement("table");
+    myGrid.classList.add("table", "table-bordered");
     var myGridTitle = document.createElement('caption');
     myGridTitle.innerHTML = "my grid";
-
 
     myGrid.id="myGrid";
     daGrids.appendChild(myGrid);
 
     var whereIveShot = document.createElement("table");
+    whereIveShot.classList.add("table", "table-dark",  "table-bordered")
     var whereIveShotTitle = document.createElement('caption');
     whereIveShotTitle.innerHTML = "Where I have shot";
 
     whereIveShot.id="whereIveShot";
     daGrids.appendChild(whereIveShot);
 
-    var letters = ["/", "a", "b", "c", "d", "e", "f", "g", "h"];
+    var letters = ["", "a", "b", "c", "d", "e", "f", "g", "h"];
 
     for (i=0; i< 9; i++){
         var rowMy = myGrid.insertRow(i);
@@ -257,11 +178,9 @@ function firstPersonView(data){
                 var cellShot = rowShot.insertCell(c);
 
                 cellMy.innerHTML=letters[c];
-                cellMy.style.color="green";
                 cellMy.style.fontWeight="bold";
 
                 cellShot.innerHTML=letters[c];
-                cellShot.style.color="green";
                 cellShot.style.fontWeight="bold";
             }
         // rest of the rows
@@ -269,12 +188,10 @@ function firstPersonView(data){
             // fill left-most cell with row number
             var cellMy = rowMy.insertCell(0);
             cellMy.innerHTML=i;
-            cellMy.style.color="green";
             cellMy.style.fontWeight="bold";
 
             var cellShot = rowShot.insertCell(0);
             cellShot.innerHTML=i;
-            cellShot.style.color="green";
             cellShot.style.fontWeight="bold";
             // fill main grid with letter/number coordinates
             for (var g=1; g<9; g++){
@@ -284,14 +201,14 @@ function firstPersonView(data){
                 for (p=0; p<data.game_player[0].ships.length; p++){
                     for (x=0; x<data.game_player[0].ships[p].location.length; x++){
                          if (data.game_player[0].ships[p].location[x] == letters[g]+i) {
-                            cellMy.style.backgroundColor = "red";
+                            cellMy.style.backgroundColor = "blue";
                          }
                          for (b=0; b<data.opponentInformation[0].enemySalvoes.length; b++){
                             for (h=0; h < data.opponentInformation[0].enemySalvoes[b].location.length; h++){
                                 if (data.opponentInformation[0].enemySalvoes[b].location[h] == letters[g]+i){
                                 cellMy.innerHTML = "x" + data.opponentInformation[0].enemySalvoes[b].turn;
                                     if (data.game_player[0].ships[p].location[x] == data.opponentInformation[0].enemySalvoes[b].location[h]){
-                                        cellMy.style.backgroundColor = "orange";
+                                        cellMy.style.backgroundColor = "red";
                                     }
                                 }
                             }
@@ -306,7 +223,7 @@ function firstPersonView(data){
                                 if (data.game_player[0].salvoes[b].location[h] == letters[g]+i){
                                 cellShot.innerHTML = "x" + data.game_player[0].salvoes[b].turn;
                                     if (data.opponentInformation[0].enemyShips[p].location[x] == data.game_player[0].salvoes[b].location[h]){
-                                        cellShot.style.backgroundColor = "orange";
+                                        cellShot.style.backgroundColor = "red";
                                     }
                                 }
                             }
